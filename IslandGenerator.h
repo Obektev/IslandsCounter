e93b75ed047ce94e0f49bcc5422bcebda6282cc5
Obektev <60030453+Obektev@users.noreply.github.com>
2024-04-19 08:35:03 +0300

    Create IslandGenerator.h

---
+++ IslandGenerator.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include<bits/stdc++.h>
+class IslandGenerator
+{
+public:
+	template<typename T>
+	vector<vector<T>> generateIslands(int x_size, int y_size, T empty_, T full_) {
+		srand(time(NULL));
+		vector<vector<T>> res(x_size, vector<T>(y_size, empty_));
+		for (int i = 0; i < x_size; i++) {
+			for (int j = 0; j < y_size; j++) {
+				if (rand() % 2 == 0)
+					res[i][j] = empty_;
+				else
+					res[i][j] = full_;
+			}
+		}
+		return res;
+	}
+};
+
